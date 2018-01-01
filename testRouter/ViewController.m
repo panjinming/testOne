d074a0725a8b90f1b6e8fb4860d90e7fd377710f
panjinming <1462299519@qq.com>
2018-01-02 00:28:04 +0800

    Revert "1"
    This reverts commit f24771822101f93f9e33455816a0af818ceb5831.

--- testRouter/ViewController.m
+++ testRouter/ViewController.m
@@ -16,11 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"1");
-    
-    
-    
-    
     NSLog(@"ewtwwt");
     NSLog(@"99999");
     // Do any additional setup after loading the view, typically from a nib.
