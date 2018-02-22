1f4df78aa7083b1b8395bebad4992a52f5238338
panjinming <1462299519@qq.com>
2018-02-22 15:33:15 +0800

    1.1r

--- testRouter/ViewController.m
+++ testRouter/ViewController.m
@@ -16,19 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
- 
-    // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"were");
-    NSDate *curr  = [[NSDate alloc] init];
-    NSTimeInterval currnettime = [curr timeIntervalSince1970];
-    NSLog(@"%lf",currnettime);
-    //1511924729.728362
-    NSTimeInterval ss = 1511937340;    //14.36.3   14.44
-    NSDate * currn = [NSDate dateWithTimeIntervalSinceNow:ss];
-    NSDateFormatter *form = [[NSDateFormatter alloc]init];
-    [form setDateFormat:@"YYYY MMM HH mm : ss"];
-    NSString *str = [form stringFromDate:currn];
-    
     
 }
 
